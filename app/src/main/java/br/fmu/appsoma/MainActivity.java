ac65b1b14c666fe2d524c80e0fe0144a1ee50b67
1804989 <1804989@fmu.edu.br>
2023-09-20 21:10:45 -0300

    Segunda tela, botão outra atividade

--- app/src/main/java/br/fmu/appsoma/MainActivity.java
+++ app/src/main/java/br/fmu/appsoma/MainActivity.java
@@ -2,6 +2,7 @@ package br.fmu.appsoma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -29,5 +30,16 @@ public class MainActivity extends AppCompatActivity {
         soma = x + y;
         editTextNumber3.setText(Integer.toString(soma));
 
+    }
+
+    public void abrirJanelaNova(View view){
+        int x, y, soma;
+        x = Integer.parseInt(editTextNumber.getText().toString());
+        y = Integer.parseInt(editTextNumber2.getText().toString());
+        soma = x + y;
+
+        Intent intent = new Intent( this, MainActivity2.class);
+        intent.putExtra("soma",soma);
+        startActivity(intent);
     }
 }
